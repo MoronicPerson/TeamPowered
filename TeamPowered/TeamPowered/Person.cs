31210fd8bc941bd691dcd4b0f48a4f122c6b88b3
seanie <seanie@LAPTOP>
2016-10-16 13:29:02 +0100

    split name into first and last name

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string name { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string address { get; set; }
     }
 }
